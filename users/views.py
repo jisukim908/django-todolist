eb0367e2cd3fdbf136c3fe3ecf9ec36edc53f21f
jisu <onpaso53@gmail.com>
2023-04-28 11:45:16 +0900

    make user everything done

--- users/views.py
+++ users/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from users.serializers import UserSerializer, CustomTokenObtainPairSerializer, UserUpdateSerializer
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView
+)
+from users.models import User
 
 # Create your views here.
+class UserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"가입완료"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message":f"${serializer.errors}"}, status= status.HTTP_400_BAD_REQUEST)
+
+
+class UserInformationView(APIView):  
+    # 사용자 정보 조회  
+    def get(self, request, user_id):
+        return Response(UserSerializer(request.user).data, status=status.HTTP_200_OK)
+
+    # 사용자 정보 수정
+    def put(self, request, user_id):
+        if not request.user.is_authenticated:
+            return Response({"message":"로그인해주세요."},401)
+        
+        user = get_object_or_404(User,id=user_id)
+        current_user= request.user
+        if current_user.id == user_id:
+            serializer = UserUpdateSerializer(user, data = request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
+
+    # 사용자 정보 탈퇴(삭제)
+    def delete(self, request, user_id):
+        if not request.user.is_authenticated:
+            return Response({"message":"로그인해주세요."},401)
+        
+        user = get_object_or_404(User,id=user_id)
+        current_user= request.user
+        if current_user.id == user_id:
+           user.delete()
+           return Response("탈퇴했습니다.", status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
+            
+
+
+# customize token
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+
+# 실제로 로그인이 되었는지 확인
+class mockView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request):
+        return Response("get 요청")
